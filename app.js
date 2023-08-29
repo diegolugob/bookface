ab624bacbdc200d8d549db9de27a3f1cc5b2b92f
diegolugob <alejandroleal246@gmail.com>
2023-08-29 00:39:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+function toggleForm() {
+	const loginForm = document.getElementById('loginForm');
+	const registerForm = document.getElementById('registerForm');
+
+	if (loginForm.style.display === 'none') {
+		loginForm.style.display = "block";
+		registerForm.style.display = 'none';
+	}else{
+		loginForm.style.display = 'none';
+		registerForm.style.display = 'block'
+	}
+}
+
+function registerUser() {
+	const username = document.getElementById('registerUsername').value;
+	const password = document.getElementById('registerPassword').value;
+
+
+//Validaciones 
+if (username.trim() === '' || password.trim() === '') {
+alert('escribe en las dos')
+
+}
+
+//guardar el usuario en el almacenamiento local
+localStorage.setItem('username', username);
+localStorage.setItem('password', password);
+
+//Mostrar la alerta de q se pudo
+alert('Se registró');
+return false;
+}
+
+function loginUser() {
+	const username = document.getElementById('loginUsername').value;
+
+	const password = document.getElementById('loginPassword').value;
+
+if (username.trim() === '' || password.trim() === '') {
+	alert('escribe en las dos')
+
+}
+
+//guardar el usuario en el almacenamiento local
+const storedUsername = localStorage.getItem('username', username);
+const storedPassword = localStorage.getItem('password', password);
+
+if (username === storedUsername && password === storedPassword) {
+
+	alert("Hey!, sí te pusdiste loggear. " + username + "Bienvenido");
+}else{
+	alert("Hey!, no se pudo ");
+}
+return false;
+
+
+}
+
+
+
